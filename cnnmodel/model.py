32b57b42379753551c42ac128f900f435cb968a7
donshen <don.z.shen@gmail.com>
2021-10-07 10:46:48 -0500

    add a sample visualization of point cloud voxelization and FT of the voxels

--- cnnmodel/model.py
+++ cnnmodel/model.py
@@ -1,45 +1,41 @@
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.optim import *
 
+# Specify number of classes
 num_classes = 9
 
-# Create CNN Model
+# CNN Model
 class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
         
-        self.conv_layer1 = self._conv_layer_set(1, 16)
-        self.conv_layer2 = self._conv_layer_set(16, 64)
-        self.fc1 = nn.Linear(2**3*64, 128)
+        self.conv_layer1 = self._conv_layer(1, 16)
+        self.conv_layer2 = self._conv_layer(16, 64)
+        self.fc1 = nn.Linear(2 ** 3 * 64, 128)
         self.fc2 = nn.Linear(128, num_classes)
         self.relu = nn.LeakyReLU()
         self.batch=nn.BatchNorm1d(128)
         self.drop=nn.Dropout(p=0.15)        
         
-    def _conv_layer_set(self, in_c, out_c):
+    def _conv_layer(self, in_c, out_c):
         conv_layer = nn.Sequential(
-        nn.Conv3d(in_c, out_c, kernel_size=(3, 3, 3), padding=0),
-        nn.LeakyReLU(),
-        nn.MaxPool3d((2, 2, 2)),
+            nn.Conv3d(in_c, out_c, kernel_size=(3, 3, 3), padding=0),
+            nn.LeakyReLU(),
+            nn.MaxPool3d((2, 2, 2)),
         )
         return conv_layer
-    
-
+   
     def forward(self, x):
-        # Set 1
-#         print(x.size())
-        out = self.conv_layer1(x)
-#         print(out.size())
-        out = self.conv_layer2(out)
-#         print(out.size())
-        out = out.view(out.size(0), -1)
-        out = self.fc1(out)
-        out = self.relu(out)
-        out = self.batch(out)
-        out = self.drop(out)
-        out = self.fc2(out)
+        # print(x.size())
+        x = self.conv_layer1(x)
+        # print(x.size())
+        x = self.conv_layer2(x)
+        # print(x.size())
+        x = x.view(out.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.batch(x)
+        x = self.drop(x)
+        x = self.fc2(x)
         
-        return out
+        return x
