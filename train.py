ca614391c5f81e515ae99149823929502e7f78fb
donshen <don.z.shen@gmail.com>
2021-10-27 10:30:26 -0500

    modify train.py

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import h5py
 import argparse
 import numpy as np
@@ -27,41 +28,46 @@ parser.add_argument('-b',
                     default=128)
 opt = parser.parse_args()
 
+def load_data(data_path):
+    # Load Data
+    with h5py.File(data_path, "r") as hf:    
+        # Split the data into training/test features/targets
+        X_train = hf["X_train"][:]
+        y_train = hf["y_train"][:]
+        X_test = hf["X_test"][:]
+        y_test = hf["y_test"][:]
+
+    # Resolution of the voxels
+    res = X_train[0].shape[0]
+
+    np.array(X_train).reshape(len(X_train), 1, res, res, res)
+    np.array(X_test).reshape(len(X_test), 1, res, res, res)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    train_x = torch.from_numpy(np.array(X_train)).float()
+    train_y = torch.from_numpy(np.array(y_train)).long()
+
+    test_x = torch.from_numpy(np.array(X_test)).float()
+    test_y = torch.from_numpy(np.array(y_test)).long()
+
+    # Pytorch train and test sets
+    train = torch.utils.data.TensorDataset(train_x, train_y)
+    test = torch.utils.data.TensorDataset(test_x, test_y)
+    return train, test
+
 # Select device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Load Data
-with h5py.File(opt.proc_path, "r") as hf:    
-    # Split the data into training/test features/targets
-    X_train = hf["X_train"][:]
-    y_train = hf["y_train"][:]
-    X_test = hf["X_test"][:]
-    y_test = hf["y_test"][:]
-
-# Resolution of the voxels
-res = X_train[0].shape[0]
-    
-np.array(X_train).reshape(len(X_train), 1, res, res, res)
-np.array(X_test).reshape(len(X_test), 1, res, res, res)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-
-train_x = torch.from_numpy(np.array(X_train)).float()
-train_y = torch.from_numpy(np.array(y_train)).long()
-
-test_x = torch.from_numpy(np.array(X_test)).float()
-test_y = torch.from_numpy(np.array(y_test)).long()
-
-# Pytorch train and test sets
-train = torch.utils.data.TensorDataset(train_x,train_y)
-test = torch.utils.data.TensorDataset(test_x,test_y)
+# load data from preprocessed path
+train, test = load_data(opt.proc_path)
 
 # data loader
-train_loader = torch.utils.data.DataLoader(train, batch_size = opt.batch_size, shuffle = True)
-test_loader = torch.utils.data.DataLoader(test, batch_size = opt.batch_size, shuffle = True)
+train_loader = torch.utils.data.DataLoader(train, opt.batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test, opt.batch_size, shuffle=True)
 
 #Definition of hyperparameters
-n_iters = (len(train_x) / opt.batch_size) * opt.num_epochs
+n_iters = (len(train) / opt.batch_size) * opt.num_epochs
 
 # Load CNNModel
 model = CNNModel().to(device)
@@ -72,9 +78,9 @@ print(model)
 loss_fn = nn.CrossEntropyLoss()
 
 # Adam
-learning_rate = 0.0001
+learning_rate = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999))
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=200, gamma=0.5)
 
 # Train
 count = 0
@@ -84,10 +90,11 @@ accuracy_list = []
 for epoch in tqdm(range(opt.num_epochs)):
     scheduler.step()
     for i, (voxels, voxel_labels) in enumerate(train_loader):
+        res_x, res_y, res_z = voxels.shape[-3:]
         voxels, voxel_labels = voxels.to(device), voxel_labels.to(device)
         if voxels.shape[0] != opt.batch_size:
             continue
-        train = Variable(voxels.view(opt.batch_size,1,res,res,res))
+        train = Variable(voxels.view(opt.batch_size, 1, res_x, res_y, res_z))
         voxel_labels = Variable(voxel_labels)
         # Clear gradients
         optimizer.zero_grad()
@@ -110,10 +117,9 @@ for epoch in tqdm(range(opt.num_epochs)):
                 voxels, voxel_labels = voxels.to(device), voxel_labels.to(device)
                 if voxels.shape[0] != opt.batch_size:
                     continue
-                test = Variable(voxels.view(opt.batch_size,1,res,res,res))
+                test = Variable(voxels.view(opt.batch_size, 1, res_x, res_y, res_z))
                 # Forward propagation
                 outputs = model(test)
-
                 # Get predictions from the maximum value
                 predicted = torch.max(outputs.data, 1)[1]
                 # Total number of labels
@@ -123,13 +129,17 @@ for epoch in tqdm(range(opt.num_epochs)):
             accuracy = 100 * correct / float(total)
             
             # store loss and iteration
-            loss_list.append(loss.data)
             iteration_list.append(count)
-            accuracy_list.append(accuracy)
+            loss_list.append(loss.data.cpu())
+            accuracy_list.append(accuracy.cpu())
  
             # Print Loss
             print(f'Iteration: {count}  Loss: {loss.data}  Accuracy: {accuracy}')
-     
+            
+        # Save plots for loss and accuracy
+        np.savetxt(os.path.join(opt.save_path, 'loss_acc.txt'),
+                   np.array((iteration_list, loss_list, accuracy_list)).T, fmt='%10.5f')
+              
     # Save the trained model
     if epoch % 100 == 0:
         torch.save(model, opt.save_path + '/' + str(epoch) + '.pth')
